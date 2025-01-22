c3f20479370518922fb81db809b724ee7c3b7878
Heavy2beat <fabianfischer81@gmx.de>
2025-01-22 16:15:50 +0100

    next try to run on gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./app";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import End from "./pages/End";
 import Game from "./pages/Game";
 import Start from "./pages/Start";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App></App>,
